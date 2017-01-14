0fa0cd248acbe7c2be9b542fbcc169c55c0c183a
pranavj1001 <pranavj1001@gmail.com>
2017-01-14 15:24:59 +0530

    add search function()

--- java/Spider.java
+++ java/Spider.java
@@ -20,5 +20,33 @@ public class Spider {
         this.pagesVisited.add(nextUrl);
         return nextUrl;
     }
+    
+    public void search(String url, String searchWord)
+    {
+        while(this.pagesVisited.size() < MAX_PAGES_TO_SEARCH)
+        {
+            String currentUrl;
+            SpiderLeg leg = new SpiderLeg();
+            if(this.pagesToVisit.isEmpty())
+            {
+                currentUrl = url;
+                this.pagesVisited.add(url);
+            }
+            else
+            {
+                currentUrl = this.nextUrl();
+            }
+            leg.crawl(currentUrl); // Lots of stuff happening here. Look at the crawl method in
+                                   // SpiderLeg
+            boolean success = leg.searchForWord(searchWord);
+            if(success)
+            {
+                System.out.println(String.format("**Success** Word %s found at %s", searchWord, currentUrl));
+                break;
+            }
+            this.pagesToVisit.addAll(leg.getLinks());
+        }
+        System.out.println("\n**Done** Visited " + this.pagesVisited.size() + " web page(s)");
+    }
 	
 }

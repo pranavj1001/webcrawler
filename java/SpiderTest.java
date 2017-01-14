61a4939de4b8e4be0dc51f799a29eae42616e8a3
pranavj1001 <pranavj1001@gmail.com>
2017-01-14 15:55:50 +0530

    test the web crawler

--- java/SpiderTest.java
+++ java/SpiderTest.java
@@ -1,5 +1,8 @@
 package webcrawler.java;
 
 public class SpiderTest {
-
+	public static void main(String[] args){
+        Spider spider = new Spider();
+        spider.search("https://github.com/pranavj1001", "webcrawler");
+    }
 }

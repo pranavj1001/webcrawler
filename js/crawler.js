15105a7d5bb0a5438769bed9504dc26d6c4d9215
pranavj1001 <pranavj1001@gmail.com>
2017-04-20 01:10:16 +0530

    add visitPage function

--- js/crawler.js
+++ js/crawler.js
@@ -2,20 +2,40 @@ var request = require('request');
 var cheerio = require('cheerio');
 var URL = require('url-parse');
 
-var pageToVisit = "https://pranavj1001.github.io";
-console.log("Visiting page " + pageToVisit);
-request(pageToVisit, function(error, response, body) {
-   if(error) {
-     console.log("Error: " + error);
-   }
-   // Check status code (200 is HTTP OK)
-   console.log("Status code: " + response.statusCode);
-   if(response.statusCode === 200) {
+var START_URL = "https://pranavj1001.github.io";
+var MAX_PAGES_TO_VISIT = 10;
+
+var pagesVisited = {};
+var numPagesVisited = 0;
+var pagesToVisit = [];
+var url = new URL(START_URL);
+var baseUrl = url.protocol + "//" + url.hostname;
+
+pagesToVisit.push(START_URL);
+
+function visitPage(url, callback) {
+
+  // Make the request
+  console.log("Visiting page " + url);
+  request(url, function(error, response, body) {
+     // Check status code (200 is HTTP OK)
+     console.log("Status code: " + response.statusCode);
+     if(response.statusCode !== 200) {
+       callback();
+       return;
+     }
      // Parse the document body
      var $ = cheerio.load(body);
-     console.log("Page title:  " + $('title').text());
-   }
-});
+     var isWordFound = searchForWord($, SEARCH_WORD);
+     if(isWordFound) {
+       console.log('Word ' + SEARCH_WORD + ' found at page ' + url);
+     } else {
+       collectInternalLinks($);
+       // In this short program, our callback is just calling crawl()
+       callback();
+     }
+  });
+}
 
 function searchForWord($, word) {
 
@@ -31,5 +51,5 @@ function collectInternalLinks($) {
     relativeLinks.each(function() {
         pagesToVisit.push(baseUrl + $(this).attr('href'));
     });
-    
+
 }
